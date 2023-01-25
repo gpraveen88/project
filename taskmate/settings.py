c22f263163ca87c5d0647db3e97ce01733c1072b
gpraveen88 <122886565+gpraveen88@users.noreply.github.com>
2023-01-25 21:56:04 +0530

    Update settings.py

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4w)fsas-9&py(7hs8a0dm@dtkwkgkw3c8&6s9o8x1ylr_j4xlv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [http://gpraveen.pythonanywhere.com/]
 
 
 # Application definition
